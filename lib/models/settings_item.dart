2810f2d41d3453bfeeb87be9f07ee58f2d477b9c
Eduardo Mollo <eduardo.mollo@outlook.it>
2025-11-26 17:41:27 +0100

    feat: setting item on tap event

--- lib/models/settings_item.dart
+++ lib/models/settings_item.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 class CuReSettingsItem {
   final IconData? icon;
   final Widget? iconChild;
-  final String description;
+  final String? description;
   final Widget child;
   final Color? color;
+  final VoidCallback? onTap;
 
   CuReSettingsItem({
     this.icon,
     this.iconChild,
-    required this.description,
+    this.description,
     required this.child,
     this.color,
+    this.onTap,
   });
 }

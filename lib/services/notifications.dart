0ef5889a8872c377a0f9beec277112117c36c243
Eduardo Mollo <eduardo.mollo@outlook.it>
2026-03-02 09:30:31 +0100

    fix: notifications custom key

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -4,13 +4,22 @@ import 'package:flutter/material.dart';
 
 class CuReNotifications {
   static GlobalKey<CuReScaffoldState> key = GlobalKey<CuReScaffoldState>();
+  static Map<String, GlobalKey<CuReScaffoldState>> keys = {};
 
   static void show(LocalNotification notification) {
+    if (notification.scaffoldKey != null) {
+      notification.scaffoldKey?.currentState?.enqueue(notification);
+      return;
+    }
     key.currentState?.enqueue(notification);
   }
 
   static void generateKey() {
     key = GlobalKey<CuReScaffoldState>();
+  }
+
+  static GlobalKey<CuReScaffoldState> getRandomKey() {
+    return GlobalKey<CuReScaffoldState>();
   }
 }
 
